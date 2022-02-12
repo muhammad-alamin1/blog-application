c28c84e8674ffc20aff37128641d6d05c873b1c2
muhammad <muhammad@kali.kali>
2022-02-13 04:16:23 +0600

    user schema

---
+++ models/User.js
@@ -0,0 +1,30 @@
+
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        maxLength: 30,
+        required: true
+    }, 
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profile: {
+        type: Schema.Types.ObjectId,
+        ref: 'Profile'
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
