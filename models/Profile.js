5746dccfab03b878e6bc4e971e82c8aacfda0a7a
muhammad <muhammad@kali.kali>
2022-02-16 10:35:07 +0600

    Schema model complete

---
+++ models/Profile.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const User = require('./User');
+const Post = require('./Post');
+
+const profileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    title: {
+        type: String,
+        trim: true,
+        maxLength: 100
+    },
+    bio: {
+        type: String,
+        trim: true,
+        maxLength: 500
+    },
+    profilePic: String,
+    links: {
+        website: String,
+        facebook: String,
+        twitter: String,
+        github: String
+    },
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: Post
+    }],
+    bookmarks: [{
+        type: Schema.Types.ObjectId,
+        ref: Post
+    }]
+
+}, { timestamps: true });
+
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
