d0ed62406d8baf3465a286e5ee01ddc903c774f2
Muhammad <muhammad@kali.kali>
2022-03-04 21:59:34 +0600

    updated file some change

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,6 +1,6 @@
 const postRouter = require('express').Router({ caseSensitive: true });
 
-const { createPostGetController, createPostPostController, editPostGetController } = require('../controllers/postController');
+const { createPostGetController, createPostPostController, editPostGetController, editPostPostController } = require('../controllers/postController');
 const { isAuthenticated } = require('../middleware/authMiddleware');
 const createPostValidator = require('../validator/postValidator');
 const upload = require('../middleware/uploadMiddleware');
@@ -8,5 +8,6 @@ const upload = require('../middleware/uploadMiddleware');
 postRouter.get('/create', isAuthenticated, createPostGetController);
 postRouter.post('/create', isAuthenticated, upload.single('post-thumbnail'), createPostValidator, createPostPostController);
 postRouter.get('/edit/:postId', isAuthenticated, editPostGetController);
+postRouter.post('/edit/:postId', isAuthenticated, upload.single('post-thumbnail'), createPostValidator, editPostPostController);
 
 module.exports = postRouter;
