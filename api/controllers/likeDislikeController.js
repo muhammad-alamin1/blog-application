2923ba4860d794e7dbc6fb48617b1deead5fdf5e
Muhammad <muhammad@kali.kali>
2022-03-06 21:38:14 +0600

    dislikes

--- api/controllers/likeDislikeController.js
+++ api/controllers/likeDislikeController.js
@@ -3,7 +3,7 @@ const Post = require("../../models/Post");
 // post Likes
 const postLikesController = async(req, res, next) => {
     const { postId } = req.params;
-    const userId = req.user.id;
+    const userId = req.user._id;
     let liked = null;
 
     if (!req.user) {
@@ -20,9 +20,18 @@ const postLikesController = async(req, res, next) => {
 
         if (post.likes.includes(userId)) {
             await Post.findOneAndUpdate({ _id: postId }, { $pull: { 'likes': userId } });
+            liked = false;
         } else {
             await Post.findOneAndUpdate({ _id: postId }, { $push: { 'likes': userId } });
+            liked = true;
         }
+
+        let updatedPost = await Post.findById(postId);
+        res.status(200).json({
+            liked,
+            totalLikes: updatedPost.likes.length,
+            totalDislikes: updatedPost.dislikes.length
+        })
 
     } catch (error) {
         console.log(error);
@@ -34,7 +43,43 @@ const postLikesController = async(req, res, next) => {
 
 // dislikes post controller
 const dislikePostController = async(req, res, next) => {
+    const { postId } = req.params;
+    const userId = req.user._id;
+    let disliked = null;
 
+    if (!req.user) {
+        return res.status(403).json({
+            error: 'You are not an authenticated user'
+        })
+    }
+
+    try {
+        let post = Post.findById(postId);
+        if (post.likes.includes(userId)) {
+            await Post.findOneAndUpdate({ _id: postId }, { $pull: { 'likes': userId } });
+        }
+
+        if (post.dislikes.includes(userId)) {
+            await Post.findByIdAndUpdate({ _id: postId }, { $pull: { 'dislikes': userId } });
+            disliked = false;
+        } else {
+            await Post.findOneAndUpdate({ _id: postId }, { $push: { 'dislikes': userId } });
+            disliked = true;
+        }
+
+        let updatedPost = await Post.findById(postId);
+        res.status(200).json({
+            disliked,
+            totalLikes: updatedPost.likes.length,
+            totalDislikes: updatedPost.dislikes.length
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: 'There was an server side error.!'
+        })
+    }
 }
 
 
